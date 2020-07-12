ce89215b7c05175d4ff00a8dc2dd8e2c9bcc11e8
AdamPayzant <payzantedwardiv@gmail.com>
2020-07-12 18:52:49 -0400

    Finished up the basic linux UI, added binaries to flash controllers

--- linux/mainwindow.cpp
+++ linux/mainwindow.cpp
@@ -1,11 +1,18 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <typeinfo>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->LoadButton->setEnabled(false);
+    populateMCU();
+    // Populate boards is currently incomplete
+    // populateBoards();
+
 }
 
 MainWindow::~MainWindow()
@@ -17,6 +24,9 @@ MainWindow::~MainWindow()
 void MainWindow::on_OpenButton_clicked()
 {
     std::cout << "Open Clicked" << std::endl;
+    QString f = QFileDialog::getOpenFileName(this, 
+        tr("Select firmware to load"), "/home", tr("HEX Files (*.hex)"));
+    ui->FilePath->setText(f);
 }
 
 void MainWindow::on_FlashButton_clicked()
@@ -24,7 +34,39 @@ void MainWindow::on_FlashButton_clicked()
     std::cout << "Flash Clicked" << std::endl;
 }
 
-void MainWindow::on_ExitButton_clicked()
-{
-    std::cout << "Exit Clicked" << std::endl;
+void MainWindow::populateMCU() {
+    std::ifstream file("../common/mcu-list.txt");
+    std::string line;
+
+    while(getline(file, line)) {
+        if(line.length() > 0) {
+            ui->MCU_List->addItem(QString::fromStdString(line));
+        }
+    }
+}
+
+void MainWindow::populateBoards() {
+    CURL *curl;
+    CURLcode res;
+    std::string readbuffer;
+    curl = curl_easy_init();
+
+    if(curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, "http://api.qmk.fm/v1/keyboards");
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readbuffer);
+        res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+
+        //std::cout << readbuffer << std::endl;
+        // TODO: Parse JSON
+        
+    }
+
+    curl_global_cleanup();
+}
+
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp) {
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
 }
--- linux/mainwindow.h
+++ linux/mainwindow.h
@@ -1,8 +1,14 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
+#include <fstream>
 #include <iostream>
+#include <string>
+#include <curl/curl.h>
+#include <json-c/json.h>
+
+#include <QMainWindow>
+#include <QFileDialog>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -21,9 +27,12 @@ private slots:
 
     void on_FlashButton_clicked();
 
-    void on_ExitButton_clicked();
-
 private:
     Ui::MainWindow *ui;
+    void populateMCU();
+    void populateBoards();
 };
+
+static size_t WriteCallback(void *, size_t, size_t, void *);
+
 #endif // MAINWINDOW_H
